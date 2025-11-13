b04cb39b780e008143591d5b22aaecac01084eb1
Finlitvin <ros.litvin@gmail.com>
2025-11-13 21:51:54 +0300

    feat: Добавлена аттака

--- src/player/player.cpp
+++ src/player/player.cpp
@@ -5,15 +5,16 @@
 
 
 Player::Player(int x, int y, char symbol)
-: MapItem(x, y, symbol), health_(100), damage_(5), armor_(600) {}
+: MapItem(x, y, symbol), health_(100), damage_(30), armor_(50) {}
 
 void Player::setHealth(int health) {
     if (health_ == 0) {
         return;
     }
 
-    if (health_ - health < 0) {
+    if ((health_ - health) < 0) {
         health_ = 0;
+        return;
     }
 
     health_ = health;
@@ -62,11 +63,22 @@ void Player::move(char key) {
 }
 
 void Player::attack(Player &enemy) {
-    if (isEnemyAround(enemy)) {
-        float health = enemy.getHealth() - damage_;
-
-        enemy.setHealth(health);
+    if (!isEnemyAround(enemy)) {
+        return;
     }
+
+    float health = enemy.getHealth();
+    float damage_cf = static_cast<float>(damage_) * 40 / 100;
+    float armor_cf = static_cast<float>(enemy.getArmor()) * 20 / 100;
+
+    float decrease_health = damage_cf - armor_cf;
+
+    if (decrease_health <= 0) {
+        decrease_health = 1;
+    }
+
+    health -= decrease_health;
+    enemy.setHealth(health);
 }
 
 bool Player::isEnemyAround(Player &enemy) {
